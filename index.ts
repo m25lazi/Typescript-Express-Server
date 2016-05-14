626a707f40355cf93a38f45d639a8a9449813018
Mohammed Lazim <mlazim@vmware.com>
2016-05-14 21:40:14 +0530

    adding port

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@
 import express = require("express");
 var app = express();
 
-app.listen(3000, ()=>{
+var port = parseInt(process.env.PORT, 10) || 3000;
+app.listen(port, ()=>{
     console.log("Started");
 })
 
