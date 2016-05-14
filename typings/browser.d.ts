a4fe4316185039146c8893e49ade943a2bce670d
Mohammed Lazim <mlazim@vmware.com>
2016-05-14 21:34:47 +0530

    adding mime typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="browser/ambient/express/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
 /// <reference path="browser/ambient/serve-static/index.d.ts" />
+/// <reference path="browser/definitions/mime/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="main/ambient/express/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/ambient/serve-static/index.d.ts" />
+/// <reference path="main/definitions/mime/index.d.ts" />
